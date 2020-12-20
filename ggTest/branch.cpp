839c774f57425621c6949b748f7ecd5a97d5543e
ab167943 <ab167943@gmail.com>
2020-12-20 17:48:10 +0800

    b/bl/bx instruction test ready

--- ggTest/branch.cpp
+++ ggTest/branch.cpp
@@ -44,4 +44,79 @@ namespace {
         TEST_LOOPS(TestMain, offsetValue);
         fmt::print("Total performed tests: {}\n", t);
     }
+
+    TEST_F(ggTest, bl_test) {
+        using namespace gg_core;
+
+        Arm egg;
+        gg_core::GbaInstance instance(std::nullopt);
+        ArmAssembler gg_asm;
+
+        unsigned int t = 0;
+        TestField offsetValue(0, 0xffffff, 1);
+
+        auto TestMain = [&]() {
+            ++t;
+
+            uint32_t instruction = MakeBranchInstruction<Cond, L, Offset>(
+                    AL, true, offsetValue.value
+            );
+
+            egg.regs[15] = 0 ;
+            instance._status._regs[15] = 0 ;
+
+            uint32_t inst_hash = hashArm(instruction);
+
+            std::invoke(egg.instr_arm[inst_hash], &egg, instruction);
+            instance.CPUTick_Debug(instruction);
+
+            uint32_t errFlag = CheckStatus(instance, egg);
+            ASSERT_TRUE(errFlag == 0)
+                                        << "#" << t << '\n'
+                                        << std::hex << "Errflag: " << errFlag << '\n'
+                                        << fmt::format("Testcase: offset_raw: {:x}\n", offsetValue.value)
+                                        << gg_asm.DASM(instruction) << "[" << instruction << "]" << '\n'
+                                        << Diagnose(instance, egg, errFlag);
+        };
+
+        TEST_LOOPS(TestMain, offsetValue);
+        fmt::print("Total performed tests: {}\n", t);
+    }
+
+    TEST_F(ggTest, bx_test) {
+        using namespace gg_core;
+
+        Arm egg;
+        gg_core::GbaInstance instance(std::nullopt);
+        ArmAssembler gg_asm;
+
+        unsigned int t = 0;
+        TestField targetRn(0, 0xe, 1); // Let's check bx r15 behavior in another test.
+        TestField RnValue(0x2000000, 0x203ffff, 1); // jump only in WRAM
+
+        auto TestMain = [&]() {
+            ++t;
+
+            uint32_t instruction = 0xe12fff10 | targetRn.value ;
+
+            egg.regs[targetRn.value] = RnValue.value ;
+            instance._status._regs[targetRn.value] = RnValue.value ;
+
+            uint32_t inst_hash = hashArm(instruction);
+
+            std::invoke(egg.instr_arm[inst_hash], &egg, instruction);
+            instance.CPUTick_Debug(instruction);
+
+            uint32_t errFlag = CheckStatus(instance, egg);
+            ASSERT_TRUE(errFlag == 0)
+                                        << "#" << t << '\n'
+                                        << std::hex << "Errflag: " << errFlag << '\n'
+                                        << fmt::format("Testcase: Rn={:x}", RnValue.value)
+                                        << gg_asm.DASM(instruction) << "[" << instruction << "]" << '\n'
+                                        << Diagnose(instance, egg, errFlag);
+        };
+
+        TEST_LOOPS(TestMain, targetRn, RnValue);
+        fmt::print("Total performed tests: {}\n", t);
+    }
 }
