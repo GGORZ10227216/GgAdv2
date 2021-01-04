30a33ee018adcbde9935db16cbc4077005562383
orzgg <ab167943@gmail.com>
2021-01-04 18:20:48 +0800

    Add ldrb imm offset test

--- ggTest/memory_read.cpp
+++ ggTest/memory_read.cpp
@@ -69,6 +69,58 @@ namespace {
         std::cout << "Test performed: " << t << std::endl ;
     }
 
+    TEST_F(ggTest, ldrb_post_imm_offset_test) {
+        Arm egg;
+        gg_core::GbaInstance instance(std::nullopt);
+        ArmAssembler gg_asm ;
+
+        unsigned int t = 0 ;
+        TestField targetRn(0, 0xf, 1) ;
+        TestField targetRd(0, 0xf, 1) ;
+        TestField immOffset(0, 0xfff, 1) ;
+        TestField memValueidx(0, 3, 1) ;
+
+        TestField uFlag(0, 1, 1) ;
+        TestField wFlag(0, 1, 1) ;
+
+        auto TestMain = [&]() {
+            ++t ;
+
+            if (wFlag.value && targetRn.value == pc)
+                return ;
+
+            uint32_t targetAddr = baseAddr ;
+
+            uint32_t instruction = MakeSingleTransferInstruction<Cond, F_Type::I, P, U, B, W, L, Rn, Rd, F_Type::Imm>(
+                    AL, false, false, uFlag.value, true, wFlag.value, true,
+                    targetRn.value, targetRd.value, immOffset.value
+            ) ;
+
+            instance._status._regs[ targetRn.value ] = baseAddr ;
+            egg.regs[ targetRn.value ] = baseAddr ;
+
+            instance._mem.Write32(targetAddr, testValue[memValueidx.value]) ;
+            egg.writeWord(targetAddr, testValue[memValueidx.value]) ;
+
+            uint32_t inst_hash = hashArm(instruction) ;
+
+            std::invoke(egg.instr_arm[inst_hash], &egg, instruction);
+            instance.CPUTick_Debug(instruction);
+
+            uint32_t errFlag = CheckStatus(instance, egg) ;
+            uint32_t memReadBack = gg_core::rotr(testValue[memValueidx.value], (targetAddr & 0b11) << 3);
+            ASSERT_TRUE(errFlag == 0)
+                                        << "#" << t << '\n'
+                                        << std::hex << "Errflag: " << errFlag << '\n'
+                                        << fmt::format( "Testcase: offset: {:x}\n", immOffset.value)
+                                        << gg_asm.DASM(instruction) << "[" << instruction << "]" << '\n'
+                                        << Diagnose(instance, egg, errFlag) ;
+        };
+
+        TEST_LOOPS(TestMain, uFlag, wFlag, targetRn, targetRd, immOffset, memValueidx) ;
+        std::cout << "Test performed: " << t << std::endl ;
+    }
+
     TEST_F(ggTest, ldr_pre_imm_offset_test) {
         Arm egg;
         gg_core::GbaInstance instance(std::nullopt);
@@ -97,6 +149,62 @@ namespace {
 
             uint32_t instruction = MakeSingleTransferInstruction<Cond, F_Type::I, P, U, B, W, L, Rn, Rd, F_Type::Imm>(
                     AL, false, true, uFlag.value, false, wFlag.value, true,
+                    targetRn.value, targetRd.value, immOffset.value
+            ) ;
+
+            instance._status._regs[ targetRn.value ] = baseAddr ;
+            egg.regs[ targetRn.value ] = baseAddr ;
+
+            instance._mem.Write32(targetAddr, testValue[memValueidx.value]) ;
+            egg.writeWord(targetAddr, testValue[memValueidx.value]) ;
+
+            uint32_t inst_hash = hashArm(instruction) ;
+
+            std::invoke(egg.instr_arm[inst_hash], &egg, instruction);
+            instance.CPUTick_Debug(instruction);
+
+            uint32_t errFlag = CheckStatus(instance, egg) ;
+            uint32_t memReadBack = gg_core::rotr(testValue[memValueidx.value], (targetAddr & 0b11) << 3);
+            ASSERT_TRUE(errFlag == 0)
+                                        << "#" << t << '\n'
+                                        << std::hex << "Errflag: " << errFlag << '\n'
+                                        << fmt::format( "Testcase: offset: {:x}\n", immOffset.value)
+                                        << gg_asm.DASM(instruction) << "[" << instruction << "]" << '\n'
+                                        << Diagnose(instance, egg, errFlag) ;
+        };
+
+        TEST_LOOPS(TestMain, uFlag, wFlag, targetRn, targetRd, immOffset, memValueidx) ;
+        std::cout << "Test performed: " << t << std::endl ;
+    }
+
+    TEST_F(ggTest, ldrb_pre_imm_offset_test) {
+        Arm egg;
+        gg_core::GbaInstance instance(std::nullopt);
+        ArmAssembler gg_asm ;
+
+        unsigned int t = 0 ;
+        TestField targetRn(0, 0xf, 1) ;
+        TestField targetRd(0, 0xf, 1) ;
+        TestField immOffset(0, 0xfff, 1) ;
+        TestField memValueidx(0, 3, 1) ;
+
+        TestField uFlag(0, 1, 1) ;
+        TestField wFlag(0, 1, 1) ;
+
+        auto TestMain = [&]() {
+            ++t ;
+
+            if (wFlag.value && targetRn.value == pc)
+                return ;
+
+            uint32_t targetAddr = baseAddr ;
+            if (uFlag.value)
+                targetAddr += immOffset.value ;
+            else
+                targetAddr -= immOffset.value ;
+
+            uint32_t instruction = MakeSingleTransferInstruction<Cond, F_Type::I, P, U, B, W, L, Rn, Rd, F_Type::Imm>(
+                    AL, false, true, uFlag.value, true, wFlag.value, true,
                     targetRn.value, targetRd.value, immOffset.value
             ) ;
 
