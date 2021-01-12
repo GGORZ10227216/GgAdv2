a3c232ad5fc3c10493ba6c8234b45336aded921e
orzgg <ab167943@gmail.com>
2021-01-12 18:42:35 +0800

    Add ldrh test

--- ggTest/ldrh.cpp
+++ ggTest/ldrh.cpp
@@ -2,3 +2,20 @@
 // Created by buildmachine on 2021-01-11.
 //
 
+#include <gg_test.h>
+
+namespace {
+    using namespace gg_core;
+    using namespace gg_core::gg_cpu;
+    using namespace gg_core::gg_mem;
+
+    uint32_t baseAddr = 0x02020000 ;
+
+    TEST_F(ggTest, ldrh_reg_offset) {
+        TestField targetRd(0, 0xf, 1) ;
+        TestField targetRn(0, 0xf, 1) ;
+        TestField targetRm(0, 0xe, 1) ;
+
+        TestField RmValue(0, 0x1ffff, 4) ;
+    }
+}
